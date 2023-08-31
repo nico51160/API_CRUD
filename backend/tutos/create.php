fe6c5bb4f17f6d1cb38f115408c2bf69261efa4e
unknown <nicolas.terry@hotmail.fr>
2023-08-31 23:29:16 +0200

    newUpdate

--- backend/tutos/create.php
+++ backend/tutos/create.php
@@ -8,22 +8,36 @@ header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Access-Contro
 ////////zone de controle
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    ////////reponse api ok 
+     ////////reponse api ok 
         http_response_code(200);
 
-///////// Traitement des infos recues
-    $data = json_decode(file_get_contents("php://input"), true);
-    if(!empty($data['titre']) && !empty($data['description']) && !empty($data['url'] )) {
+     //////// Traitement des infos recues
+        $data = json_decode(file_get_contents("php://input"), true);
 
-    } else {
-        http_response_code(405);
-        $message = array(
-            'msgErreur'   => 'une erreur est survenue',
-            'explication' => 'les champs titre, dscription et url sont obligatoires'
-        );
-        echo json_encode($message);
-    }
-///////// Traitement des infos recues
+        if( !empty($data['titre']) && !empty($data['description']) && !empty($data['url']) ) {
+
+                    ////////appel methodes
+
+            include('../cnx.php');
+            /*include('../classes/TutoManager.php');
+            include('../classes/Tuto.php');*/
+            spl_autoload_register(function($class) {
+                include('../classes/'.$class.'.php');
+            });
+
+            $manager = new TutoManager($cnx);
+            $tuto = $manager->ReadTuto($_GET['tutoID']);
+            ////////appel methodes
+
+        } else {
+            http_response_code(405);
+            $message = array(
+                'msgErreur'   => 'une erreur est survenue',
+                'explication' => 'les champs titre, dscription et url sont obligatoires'
+            );
+            echo json_encode($message);
+        }
+      //////// Traitement des infos recues
 
 
 
@@ -41,4 +55,4 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
         'explication' => 'vous devez utiliser la methode POST'
     );
     echo json_encode($message);
-    }
+}
